890affb1146bb1a16875f53264adfa451ccb2f1c
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-04-13 11:07:42 -0600

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,26 @@ import { Link } from 'react-router-dom'
 
 export default function About(){
     return(
-        <h1>This is the about page</h1>
+        <div className="about-container">
+            <header>
+                <Link className={"title-link"}to="/">#OVERLANDERS</Link>
+                <nav>
+                    <Link className={"about-link"}to="/About">About</Link>
+                    <Link className={"trucks-link"}to="/Trucks">Trucks</Link>
+                </nav>
+            </header>
+            <main>
+                <section className="image-container"></section>
+                <section className="about-info">
+                    <h1></h1>
+                    <p></p>
+                    <section className="about-action-container">
+                        <h1></h1>
+                        <button></button>
+                    </section>
+                </section>
+
+            </main>
+        </div>
     )
 }
